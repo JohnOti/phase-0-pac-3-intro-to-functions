b27cb77fc2f438d998e8d00a2b4f84dc64bf3f1d
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-02 17:00:10 +0300

    Done with js trial

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Follow along with the examples here
+function square(number) {
+    return number * number;
+  }
+  square(5)
+  console.log(square(5));
+  function say(greeting, firstName) {
+    console.log("I am coming!");
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Howdy", "partner"));
